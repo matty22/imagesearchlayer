d95a5aa544f239bd1f08aa039c12b91b5a8e6a82
Matt <matt.leonard.22@gmail.com>
2017-01-09 19:40:06 -0800

    replaced console log with response send

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/search/:searchQuery', function(req, res) {
     method: "GET",
     headers: { "Ocp-Apim-Subscription-Key": key }
   }, function(error, response, body) {
-      console.log(body);
+      response.send(body);
       });
 });
 
