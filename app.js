b7fd65a45d6cf4d651b81c5ba1f38eefa7462ccf
Matt <matt.leonard.22@gmail.com>
2017-01-10 06:09:25 -0800

    removed bracket notation as it errs the app

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/search/:searchQuery', function(req, res) {
     method: "GET",
     headers: { "Ocp-Apim-Subscription-Key": key }
   }, function(error, response, body) {
-      var resultsArray = body[value];
+      var resultsArray = body;
       console.log(resultsArray);
       res.send(body);
       });
